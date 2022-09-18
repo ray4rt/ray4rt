4bd0b1bb1600701b943d13b83da4887d32aafabb
ray4rt <86026143+ray4rt@users.noreply.github.com>
2022-09-18 16:28:54 +0700

    Create track.py

---
+++ track.py
@@ -0,0 +1,34 @@
+GNU nano 6.3              denz.py
+import os,sys,time
+def tod(z):
+
+
+        for e in z + "\n":
+                sys.stdout.write(e)
+                sys.stdout.flush()
+        time.sleep(1)
+tod("TRACK GEOLOCATION")
+tod("TARGET IP            : 124.49.100.82")
+tod("TARGET LOCK ENCRYPT  : HAhapoGYHiREhNIkkHhHbcXzs==")
+tod("LOCATION IP          : INDONESIAN")
+tod("CITY ENCRYPT         : AUafYkkwPoMbHyYdannbHtVln==")
+tod("OPERATING SYSTEM     : ANDROID,Version 11")
+tod("REQUEST URI FROM     : GET CONTACT")
+tod("TRACK MAPS           : -6.859652,207.495621")
+tod("PHONE NUMBER         : +62-897-2804-4580")
+tod("PROTOCOL USAGE       : HTTPS/WWW")
+tod("TOTAL COOKIE         : 12KB")
+tod("LOADING.........................+")
+tod("TRACK SUCCESSFULLY..............+")
+tod("PLEASE DECRYPT THE ALL TARGET INFORMATION')
+tod("LINK                 : https://base64decode.com")
+tod("BASE64,SHA1,SHA12,MD5")
+tod("VPN NO DETECTED...........")
+tod("GOOD JOBS.............")
+tod("DOWNLOADING DATA.....1")
+tod("DOWNLOADING DATA.....2")
+tod("DOWNLOADING DATA.....3")
+tod("DOWNLOADING DATA.....4")
+tod("DOWNLOADING DATA.....5")
+tod("SUCCESS DOWNLOAD ALL DATA")
+tod("SAVE TO ../Internal/Download/result.txt")
